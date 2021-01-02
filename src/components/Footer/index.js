d8d2a6d91b2b42e6a26331a7311052b5594e580d
Moaz Ahmed <moaz.ahmed132@gmail.com>
2021-01-02 02:46:06 +0200

    add the smooth scroll effect

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { animateScroll as scroll } from 'react-scroll'
 import { FaFacebook, FaInstagram, FaTwitter, FaYoutube, FaLinkedin } from 'react-icons/fa'
 import { FooterContainer, FooterWrap, FooterLinksContainer, FooterLinksWrapper, FooterLinkItems, FooterLinkTitle, FooterLink, SocialMedia, SocialMediaWrap, SocialLogo, WebsiteRights, SocialIcons, SocialIconLink } from './FooterElements'
 
 const Footer = () => {
+  const toggleHome = () => {
+    scroll.scrollToTop()
+  }
   return (
     <>
       <FooterContainer>
@@ -51,7 +55,7 @@ const Footer = () => {
 
           <SocialMedia>
             <SocialMediaWrap>
-              <SocialLogo to='/'>
+              <SocialLogo to='/' onClick={toggleHome}>
                 dolla
               </SocialLogo>
               <WebsiteRights>
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,15 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { FaBars } from 'react-icons/fa'
 import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavLinks, NavItem, NavBtn, NavBtnLink }
   from './NavbarElements'
+import { animateScroll as scroll } from 'react-scroll'
 
 
 const Navbar = ({ toggle }) => {
+  const [scrollNav, setScrollNav] = useState(false);
+
+  const changeNav = () => {
+    if (window.scrollY >= 80) {
+      setScrollNav(true);
+    } else {
+      setScrollNav(false)
+    }
+  }
+  useEffect(() => {
+    window.addEventListener('scroll', changeNav)
+  }, []);
+
+  const toggleHome = () => {
+    scroll.scrollToTop()
+  }
   return (
     <>
-      <Nav>
+      <Nav scrollNav={scrollNav}>
         <NavbarContainer>
-          <NavLogo to='/'>
+          <NavLogo to='/' onClick={toggleHome}>
             dolla
           </NavLogo>
           <MobileIcon onClick={toggle}>
@@ -17,16 +34,36 @@ const Navbar = ({ toggle }) => {
           </MobileIcon>
           <NavMenu>
             <NavItem>
-              <NavLinks to='about'>About</NavLinks>
+              <NavLinks to='about'
+                smooth={true}
+                duration={500}
+                spy={true}
+                exact='true'
+                offset={-80}>About</NavLinks>
             </NavItem>
             <NavItem>
-              <NavLinks to='discover'>Discover</NavLinks>
+              <NavLinks to='discover'
+                smooth={true}
+                duration={500}
+                spy={true}
+                exact='true'
+                offset={-80}>Discover</NavLinks>
             </NavItem>
             <NavItem>
-              <NavLinks to='services'>Services</NavLinks>
+              <NavLinks to='services'
+                smooth={true}
+                duration={500}
+                spy={true}
+                exact='true'
+                offset={-80}>Services</NavLinks>
             </NavItem>
             <NavItem>
-              <NavLinks to='signup'>Sign Up</NavLinks>
+              <NavLinks to='signup'
+                smooth={true}
+                duration={500}
+                spy={true}
+                exact='true'
+                offset={-80}>Sign Up</NavLinks>
             </NavItem>
           </NavMenu>
           <NavBtn>
